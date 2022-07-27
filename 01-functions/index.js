f400d9348920d8f236256fffa06bd09a37154420
chris <chrishethertonweb@gmail.com>
2022-07-27 10:52:10 +0200

    ex 7 Bonus done

--- 01-functions/index.js
+++ 01-functions/index.js
@@ -140,10 +140,12 @@ easyLoop()
 
 //  EX 7 
 function multiplyLoop() {
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            console.log((i * j));
 
+        }
 
-        console.log((i * 9));
     }
 
 }
